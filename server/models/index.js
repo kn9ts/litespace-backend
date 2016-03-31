1613936e1526e958f7ea2f2c592cb1cef1acd31a
Eugene Mutai <eugenemutai@gmail.com>
2016-03-31 15:54:00 +0300

    Added models index and updated routes index

---
+++ server/models/index.js
@@ -0,0 +1,21 @@
+// instantiate the database connection
+var mongoose = require('../config/database'),
+  path = require('path'),
+  ucFirst = require('../services/ucfirst'),
+  Schema = mongoose.Schema;
+
+// load models
+var models = [
+  'documents',
+  'roles',
+  'users'
+];
+
+// add them to be exported in one go
+models.forEach(function(model) {
+  module.exports[ucFirst(model)] = require(path.join(__dirname, model))(mongoose, Schema);
+});
+
+// export connection
+module.exports.mongoose = mongoose;
+module.exports.Schema = Schema;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(router) {
+  'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  /* Load up the homepage */
+  router.get('/*', function(req, res) {
+    res.sendFile('index.html', {
+      root: './public/'
+    });
+  });
 
-module.exports = router;
+  return router;
+};
+
